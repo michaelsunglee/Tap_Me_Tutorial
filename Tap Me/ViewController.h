86db1977ed145d75471f41506048f2d01d2f1f39
Michael Lee <michaelsunglee@gmail.com>
2014-09-06 03:42:23 -0400

    Used a tutorial, single and final? push

--- Tap Me/ViewController.h
+++ Tap Me/ViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<UIAlertViewDelegate>{
+    IBOutlet UILabel *scoreLabel;
+    IBOutlet UILabel *timerLabel;
+    
+    NSInteger count;
+    NSInteger seconds;
+    NSTimer *timer;
+    
+    AVAudioPlayer *buttonBeep;
+    AVAudioPlayer *secondBeep;
+    AVAudioPlayer *backgroundMusic;
+}
+-(IBAction)buttonPressed;
 @end
--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -14,10 +14,34 @@
 
 @implementation ViewController
 
+-(AVAudioPlayer *)setupAudioPlayerWithFile:(NSString *)file type:(NSString *)type{
+    NSString *path=[[NSBundle mainBundle] pathForResource:file ofType:type];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    NSError *error;
+    
+    AVAudioPlayer *audioPlayer=[[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    
+    if(!audioPlayer){
+        NSLog(@"%@", [error description]);
+    }
+    
+    return audioPlayer;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_tile.png"]];
+    scoreLabel.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"field_score.png"]];
+    timerLabel.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"field_time.png"]];
+
+    buttonBeep=[self setupAudioPlayerWithFile:@"ButtonTap" type:@"wav"];
+    secondBeep=[self setupAudioPlayerWithFile:@"SecondBeep" type:@"wav"];
+    backgroundMusic=[self setupAudioPlayerWithFile:@"HallOfTheMountainKing" type:@"mp3"];
+    
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +50,48 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)buttonPressed{
+    count++;
+    scoreLabel.text=[NSString stringWithFormat:@"Score\n%li", (long)count];
+    
+    [buttonBeep play];
+}
+
+-(void)setupGame{
+    seconds=30;
+    count=0;
+    
+    timerLabel.text=[NSString stringWithFormat:@"Time %li", (long)seconds];
+    scoreLabel.text=[NSString stringWithFormat:@"Score\n%li", (long)count];
+    
+    timer=[NSTimer scheduledTimerWithTimeInterval: 1.0f
+                                           target:self
+                                         selector:@selector(subtractTime)
+                                         userInfo:nil
+                                          repeats:YES];
+    
+    [backgroundMusic setVolume:0.3];
+    [backgroundMusic play];
+}
+
+-(void)subtractTime{
+    --seconds;
+    timerLabel.text=[NSString stringWithFormat:@"Time: %li", (long)seconds];
+    [secondBeep play];
+    
+    if(seconds == 0){
+        [timer invalidate];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                    message:[NSString stringWithFormat:@"You scored %li points", (long)count]
+                                                   delegate:self
+                                          cancelButtonTitle:@"Play again"
+                                          otherButtonTitles: nil];
+    [alert show];
+    }
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    [self setupGame];
+}
 @end
